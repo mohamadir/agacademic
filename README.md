729744ba12790e1773acf35868e90581689fb3fe
mohamed <mohamdib@gmail.com>
2017-04-30 22:15:52 +0300

    aaa

--- README.md
+++ README.md
@@ -32,6 +32,6 @@ Start Bootstrap was created by and is maintained by **[David Miller](http://davi
 
 Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
 
-## Copyright and License
+## Copyright and License 2
 
 Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-agency/blob/gh-pages/LICENSE) license.
--- app.js
+++ app.js
@@ -31,16 +31,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // session 
-/*app.use(session({
+app.use(session({
   secret: "secret",
   saveUninitialized: true,
   resave: true
 }));
-*/
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/auth', auth);
-//app.use('/admin', admin);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
